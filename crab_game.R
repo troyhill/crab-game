6f372c3d03222655c4a7508bd22628c2fdd90464
troyhill <hill.troy@gmail.com>
2014-08-19 17:14:36 -0400

    crab.game update

--- crab_game.R
+++ crab_game.R
@@ -1,19 +1,24 @@
 
-game <- function(){
+crab.game <- function(pts = 0){
   answers <- data.frame(user = c("Sesarma", "Spartina", "Callinectes"), Sesarma.value = c(0, -1, 1),
                         Spartina.value = c(1, 0, -1), Callinectes.value = c(-1, 1, 0), 
                         verb = c(" eats ", " blocks ", " devours "))
   user <- readline("
 Welcome to Sesarma-Spartina-Callinectes!
 
-Do you play Sesarma, Spartina, or Callinectes?  ")
+Sesarma, Spartina, Callinectes, shoot!  ")
   if(!user %in% answers$user) stop("You can only play 'Sesarma', 'Spartina', or 'Callinectes' ")
   comp <- answers$user[round(runif(1, 0.5, 3.5))]  # could also use sample()
   outcome.num <- answers[answers$user == user, paste0(comp, ".value")]  
+  pts <- c(pts, outcome.num)
+  score <- sum(pts)
+  invisible(score)
   outcome <- ifelse(outcome.num == 0, "It's a tie", ifelse(outcome.num == -1, paste0(comp,
-             answers$verb[answers$user == comp], user, "; you lost"), paste0(user,
-             answers$verb[answers$user == user], comp, "; you won")))
-  print(paste0("Computer played ", comp, ". ", outcome, "!"))  
+            answers$verb[answers$user == comp], user, "; you lost"), paste0(user,
+            answers$verb[answers$user == user], comp, "; you won")))
+  print(paste0("Computer played ", comp, ". ", outcome, "!"))
+  print(paste0("Current score: ", score))
   cont <- readline("Play again? y/n ")
-  if(cont == "y" | cont == "yes") game()
+  if(cont == "y" | cont == "yes") crab.game(pts = score)
+  
 }
